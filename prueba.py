a06678a78d8ece2c8836b64d48d22f5e4d1ad02b
Dherrom0706 <dionihernandezromera@gmail.com>
2022-06-07 20:52:51 +0200

    subida 2

--- prueba.py
+++ prueba.py
@@ -1,5 +1,16 @@
+#Asi se hacen los comentarios en python
+
+#Enseñar por pantalla una cadena
 print("Hola")
 print("Adios")
+
+#Declaracion de una variable y uso en un print
 prueba = "Ultim subida prueba"
 print(prueba)
-print(prueba)
+
+#Uso de un for
+a = 0
+for i in range(5):
+    a+= 1
+    print(a)
+
